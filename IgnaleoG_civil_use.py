affc00b2460d4025a1fee6d5a2b68d5385642949
Hecate2 <2474101468@qq.com>
2019-11-30 14:01:13 +0800

    Add files via upload

--- IgnaleoG_civil_use.py
+++ IgnaleoG_civil_use.py
@@ -1,10 +1,17 @@
 #coding:utf-8
-portList=[port for port in range(55568,55569)]
+#Destroyer IgnaleoG for civil use
+#驱逐舰伊格那雷奥G型民用版本
+#MIT License
+#Python 3.6 or 3.7
+#More examples will be added in the future
 
-#使用gevent使requests等socket操作异步化
 import asyncio,gevent
 from gevent import monkey
 monkey.patch_all()
+
+import functools,os,time,gc,random,re
+
+portList=tuple([i for i in range(55568,55569)])#本服务器监听端口
 
 #超时设置
 request_timeout=80
@@ -14,10 +21,33 @@ import tornado.ioloop
 import tornado.web
 from tornado.platform.asyncio import AsyncIOMainLoop
 #from concurrent.futures import ThreadPoolExecutor
-import requests
+import requests,cfscrape
 
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+from fake_useragent import UserAgent
+uaGen = UserAgent(
+    fallback="Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 SE 2.X MetaSr 1.0"
+    )
+acceptLanguage=(
+    'zh-cn,zh;q=0.8,zh-tw;q=0.7,zh-hk;q=0.5,en-us;q=0.3',
+    'zh-cn,zh;q=0.8,zh-tw;q=0.7,zh-hk;q=0.5',
+    )
+indexLanguage=len(acceptLanguage) - 1
+def genHeaders():#aiohttp会自动生成大部分header
+    headers = {
+        'User-Agent': uaGen.random,
+        'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        'Referer': "https://www.internationalsaimoe.com/voting/",
+        'Accept-Encoding': "gzip, deflate, br",
+        'Accept-Language': acceptLanguage[random.randint(0, indexLanguage)],
+        #'cache-control': "no-cache",
+        'Connection':'keep-alive',
+        'Upgrade-Insecure-Requests':'1',
+    }
+    return headers
+localsession=requests.Session()
 
 RequestExceptions=(
     requests.RequestException,
@@ -27,97 +57,18 @@ RequestExceptions=(
     )
 
 from retryapi import retry,RetryExhausted
-import gc
 
 #worker_loop=tornado.ioloop.IOLoop.instance()
 worker_loop=asyncio.get_event_loop()
 class MainHandler(tornado.web.RequestHandler):
-    #executor = ThreadPoolExecutor(30)
-
-    class VoterMT:
-        def __init__(self,proxy,loop,localsession,id=0):
-            self.proxy=proxy
-            self.loop=loop
-            self.session=requests.Session()
-            self.localsession=localsession
-            #self.executor=executor
-            self.id=id #用于标记这次投票是第几次
-
-        @retry(exceptions=RequestExceptions,tries=2,logger=None)
-        def _get(self,url,timeout=request_timeout):
-            with self.session.get(url,timeout=timeout,proxies={"http":self.proxy,"https":self.proxy},verify=False) as response:
-                if (response.status_code<400):
-                    if 'text' in response.headers['content-type']:
-                        #f=open('./tmp.txt','a',encoding='utf-8')
-                        #f.write(text)
-                        #f.close()
-                        return response.text
-                    #if 'image' in response.content_type:
-                    else:
-                        #fb=open('./tmp.png','wb')
-                        #fb.write(body)
-                        #fb.close()
-                        return response.content
-                #if (response.status==503):
-                    #pass
-                    #处理cloudflare防火墙
-                else:
-                    #print(response.status_code)
-                    return response.raise_for_status()
-
-        @retry(exceptions=RequestExceptions,tries=2,logger=None)
-        def _localget(self,url,timeout=request_timeout):
-            with self.session.get(url,timeout=timeout) as response:
-                if (response.status_code<400):
-                    if 'text' in response.headers['content-type']:
-                        #f=open('./tmp.txt','a',encoding='utf-8')
-                        #f.write(text)
-                        #f.close()
-                        return response.text
-                    #if 'image' in response.content_type:
-                    else:
-                        #fb=open('./tmp.png','wb')
-                        #fb.write(body)
-                        #fb.close()
-                        return response.content
-                #if (response.status==503):
-                    #pass
-                    #处理cloudflare防火墙
-                else:
-                    return response.raise_for_status()
-                
-        @retry(exceptions=RequestExceptions,tries=2,logger=None)
-        def _post(self,url,data,timeout=request_timeout):
-            with self.session.post(url,data=data,timeout=timeout,proxies={"http":self.proxy,"https":self.proxy},headers={'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'},verify=False) as response:
-                if (response.status_code<400):
-                    return response.text
-                else:
-                    return response.raise_for_status()
-        
-        @retry(exceptions=RequestExceptions,tries=2,logger=None)
-        def _localpost(self,url,data,timeout=request_timeout):
-            with self.localsession.post(url,data=data,timeout=timeout,verify=False) as response:
-                if (response.status_code<400):
-                    return response.text
-                else:
-                    return response.raise_for_status()
-
     #@tornado.concurrent.run_on_executor
     def get(self, *args, **kwargs):
-        gc.collect()
-        self.write('Ignaleo: 清理内存')
-        print('Ignaleo: 清理内存')
-        
+        self.write("Destroyer IgnaleoG is ready!")
+        #gevent.spawn(a function with socket operations)
+
     #@tornado.concurrent.run_on_executor
     def post(self):
-        #gc.collect()
-        proxies=self.request.body.decode(encoding='utf-8').split('\r\n')
-        self.write('收到POST')
-        print('Ignaleo: 收到POST')
-        #print('收到POST\n',proxies)
-        for proxy in proxies:
-            pass
-            #gevent.spawn(a function with socket I/O, e.g. HTTP requests, database operations)
+        #gevent.spawn(a function with socket operations)
 
 def run_proc(port):
     AsyncIOMainLoop().install()
@@ -126,7 +77,6 @@ def run_proc(port):
     ])
     app.listen(port)
     print('DestroyerIgnaleoG@localhost:%d'%(port))
-    #worker_loop.start()
     worker_loop.run_forever()
 
 if __name__ == '__main__':
