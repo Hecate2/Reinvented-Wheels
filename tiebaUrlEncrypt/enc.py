63f16661429c69e0e2e4e0a82cc1768c276f064b
Hecate2 <2474101468@qq.com>
2019-12-15 16:27:24 +0800

    把url字符加密为中文，便于百度贴吧发帖

--- tiebaUrlEncrypt/enc.py
+++ tiebaUrlEncrypt/enc.py
@@ -19,20 +19,33 @@ logger = logging.getLogger(__name__)
 
 pswd=16384
 
+from urllib import parse
+
 def enc(str_in,pswd):
     #seed(pswd)
     li_out=[chr(ord(i)^pswd) for i in str_in]
     return ''.join(li_out)
 
 if __name__=='__main__':
-    try:
-        import pyperclip
-    except Exception as e:
-        logger.info('pip install pyperclip可享受剪贴板输入输出！')
-        os.system('pip install pyperclip')
-        import pyperclip
     sleepTime=10
     defaultFilename='input.txt'
+    logger.info(r'每个中文字将被变为类似%E6%9C的形式。这不影响url的使用')
+    print()
+
+    try:
+        import pyperclip
+        hasPyperclip=True
+    except Exception as e:
+        hasPyperclip=False
+        logger.info('没有安装pyperclip。开始尝试安装')
+        logger.info('安卓QPython无法享受pyperclip复制功能')
+        try:
+            os.system('pip3 install pyperclip')
+            import pyperclip
+            hasPyperclip=True
+        except Exception:
+            logger.info('安装pyperclip失败')
+
     if len(sys.argv)>1:
         str_in=sys.argv[1]
     else:
@@ -44,28 +57,37 @@ if __name__=='__main__':
             logger.warning('获取输入失败。请检查是否存在%s。%s秒后自动退出'%(defaultFilename,sleepTime))
             time.sleep(sleepTime)
             sys.exit(1)
-    encrypted=enc(str_in,pswd)
-    print('原字符串:%s'%(str_in))
-    print('加密字符串:%s'%(encrypted))
+            
+    parse.quote(str_in).replace('%3A',':')#中文转url编码，但不改变英文冒号
+    
+    print('原字符串:\n%s'%(str_in))
+    encrypted=enc(str_in,pswd)#加密
+    print('加密字符串:\n%s'%(encrypted))
     restored=enc(encrypted,pswd)
-    print('还原后字符串:%s'%(restored))
+    print('还原后字符串:\n%s'%(restored))
 
     print()
     if restored!=str_in:
         logger.warning("还原后字符串与原始字符串不一致")
     else:
         logger.info("还原后字符串与原字符串一致")
-    saveToFile=input("退出窗口或按Ctrl+C放弃本次结果\n输入任意内容，然后按Enter可将结果[覆盖]到encrypted.txt\n不论是否输入内容，按Enter可复制结果\n")
-    if(saveToFile):
+    if(hasPyperclip):
+        saveToFile=input("退出窗口或按Ctrl+C放弃本次结果\n按Enter可[尝试复制]结果，并将结果[覆盖输出]到encrypted.txt\n")
+        #if(saveToFile):
         with open('encrypted.txt','w',encoding=('utf-8')) as f:
             f.write(encrypted)
         logger.info('加密结果已保存到encrypted.txt')
-    try:
-        import pyperclip
-        pyperclip.copy(encrypted)
-        logger.info('复制成功')
-    except Exception as e:
-        logger.warning('复制失败：%s'%(str(e)))
-    sleepTime/=2
-    logger.info('%d秒后自动退出'%(sleepTime))
-    time.sleep(sleepTime)
+        try:
+            pyperclip.copy(encrypted)
+            logger.info('复制成功')
+        except Exception as e:
+            logger.warning('复制失败：%s'%(str(e)))
+        logger.info('程序已完成。可以按Enter退出')
+        input()
+    else:
+        saveToFile=input("退出窗口或按Ctrl+C放弃本次结果\n按Enter可将结果[覆盖]到encrypted.txt\n")
+        with open('encrypted.txt','w',encoding=('utf-8')) as f:
+            f.write(encrypted)
+        logger.info('加密结果已保存到encrypted.txt')
+        logger.info('程序已完成。可以按Enter退出')
+        input()
